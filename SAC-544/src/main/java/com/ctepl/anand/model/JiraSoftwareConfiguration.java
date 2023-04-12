f3888bb25040eb5ca11f6b400bec46b3c56ee502
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-12 18:08:04 +0530

    initial commit

---
+++ SAC-544/src/main/java/com/ctepl/anand/model/JiraSoftwareConfiguration.java
@@ -0,0 +1,72 @@
+package com.ctepl.anand.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class JiraSoftwareConfiguration {
+
+	@Id
+	private Long id;
+	
+	private String userName;
+	
+	private String title;
+	
+	private String url;
+	
+	private String repositoryName;
+	
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getRepositoryName() {
+		return repositoryName;
+	}
+
+	public void setRepositoryName(String repositoryName) {
+		this.repositoryName = repositoryName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
