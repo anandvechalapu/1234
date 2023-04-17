a59c102c55984a4feabad1465d3af8f8d781b0c6
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-17 15:12:21 +0530

    Updated

--- 1234/src/main/java/com/ctepl/anand/repository/UserRepository.java
+++ 1234/src/main/java/com/ctepl/anand/repository/UserRepository.java
@@ -1,24 +1,39 @@
-·       User should be able to add, edit and delete users.·       User should be able to search users.·       User should be able to view a list of users.·       User should be able to download the user list in .csv format.·       User should be able to move a user to archived data.·       User should be able to activate a user.·       User should be able to view paginated archived users.·       All fields should be validated.
+·       User should able to access the sub functionalities like edit, view and download.·       If user has access then user able to access this page.·       If user didn’t have access then redirect to login page.·       User name should not be empty; username should be unique.·       Password should not be empty.·       Level should not be empty.·       Parent type should not be empty.·       If parent type is W, then wholesaler should not be empty.·       If parent type is B, then branch should not be empty.·       First Name should be visible for the parent type Unaffiliated user.·       Last Name should be visible for the parent type Unaffiliated user.·       Company should be visible for the parent type Unaffiliated user.·       Postcode should be visible for the parent type Unaffiliated user.·       Telephone no should be visible for the parent type Unaffiliated user.·       Email should be valid format and email should not be empty.·       Clicking on Download Data will allow User to view the results in Excel format.·       The default screen is the active users screen, If User want to move a user to archived data, click Archive User and then Ok.·       Select Archived Users to bring up a screen showing the paginated archived users.·       User can activate a user by clicking Activate User, and User can delete a user by clicking Delete.·       Once User have made User selections click on Submit it will add/edit the publication, or click on Reset to reset the selections to the last search.
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+package com.ctepl.anand.repository;
 
-	Optional<User> findByUserName(String userName);
+import org.springframework.data.jpa.repository.JpaRepository;
 
-	List<User> findByParentType(String parentType);
+import com.ctepl.anand.model.Users;
 
-	List<User> findByWholesaler(String wholesaler);
-
-	List<User> findByBranch(String branch);
-
-	List<User> findByLevel(String level);
-
-	@Query("SELECT u FROM User u WHERE u.dateCreated > :dateCreated")
-	List<User> findByDateCreated(@Param("dateCreated") LocalDateTime dateCreated);
-
-	@Query("SELECT u FROM User u WHERE u.lastModified > :lastModified")
-	List<User> findByLastModified(@Param("lastModified") LocalDateTime lastModified);
-
-	@Query("SELECT u FROM User u WHERE u.parentType = 'U' AND u.firstName IS NOT NULL AND u.lastName IS NOT NULL AND u.company IS NOT NULL AND u.postcode IS NOT NULL AND u.telephoneNo IS NOT NULL AND u.emailAddress IS NOT NULL")
-	List<User> findUnaffiliatedUser();
+public interface UserRepository extends JpaRepository<Users, Long> {
+	
+	public Users findByUserName(String userName);
+	
+	public Users findByPassword(String password);
+	
+	public Users findByLevel(String level);
+	
+	public Users findByParentType(String parentType);
+	
+	public Users findByWholesaler(String wholesaler);
+	
+	public Users findByBranch(String branch);
+	
+	public Users findByFirstName(String firstName);
+	
+	public Users findByLastName(String lastName);
+	
+	public Users findByCompany(String company);
+	
+	public Users findByPostcode(String postcode);
+	
+	public Users findByTelephoneNo(String telephoneNo);
+	
+	public Users findByEmailAddress(String emailAddress);
+	
+	public Users findByDateCreated(String dateCreated);
+	
+	public Users findByLastModified(String lastModified);
+	
 }
