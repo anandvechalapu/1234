238bf459320d8d480728d4141d5af9e53f0aca16
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-17 15:09:36 +0530

    Updated

--- 1234/src/main/java/com/ctepl/anand/repository/BranchesRepository.java
+++ 1234/src/main/java/com/ctepl/anand/repository/BranchesRepository.java
@@ -1,16 +1,28 @@
-·       Ability to find the sub functionalities when user clicks the functionality.·       Ability to access the sub functionalities like create, edit, view and download.·       If user didn’t have permission then redirect to login page.·       User should able to select the wholesaler from the wholesalers list.·       User should able to select the NFRN Region from the NFRN Regions List.·       User should able to select the TV Region from the TV Regions List.·       User should able to select No. AAT/SAT value from dropdown.·       User should have Submit and Reset buttons on this page.·       Once User have made User selections click on Submit it will add/edit the branch, or click on Reset to reset the selections to the last search.
+·       User should be able to access the sub functionalities like create, edit, view and download.·       User should be able to download the branches as format of .csv file.·       No duplicate records should be created.·       All the fields should have validations.
 
-package com.ctepl.anand.repository;
+import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface BranchesRepository extends CrudRepository<Branches, String> {
 
-public interface BranchesRepository extends JpaRepository<Branches, String> {
-	
-	Branches findByWholesalerAndName(String wholesaler, String name);
-	Branches findByWholesalerAndNumber(String wholesaler, String number);
-	List<Branches> findByNFRNRegion(String NFRNRegion);
-	List<Branches> findByTVRegion(String TVRegion);
-	List<Branches> findByNoAATSATValuesAndActive(String NoAATSATValues, String active);
-	List<Branches> findByDateCreatedAndLastModified(Date dateCreated, Date lastModified);
-	
+    Branches findByWholesalerAndNameAndNumber(String wholesaler, String name, String number);
+
+    List<Branches> findByWholesalerAndNfrnRegionAndTvRegionAndAatSatValues(String wholesaler, String nfrnRegion, String tvRegion, String aatSatValues);
+
+    List<Branches> findByActive(String active);
+
+    List<Branches> findByDateCreated(String dateCreated);
+
+    List<Branches> findByLastModified(String lastModified);
+
+    // Save Method
+    Branches save(Branches branches);
+    
+    // Delete Method
+    void delete(Branches branches);
+    
+    // View Method
+    Branches view(Branches branches);
+    
+    // Download Method
+    void download(Branches branches);
 }
