554280c35c8f3d4a722c2fac62b11bac285325a0
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-17 15:10:45 +0530

    initial commit

---
+++ 1234/src/main/java/com/ctepl/anand/model/Branches.java
@@ -0,0 +1,68 @@
+public class Branches {
+    private String wholesaler;
+    private String name;
+    private String number;
+    private String nfrnRegion;
+    private String tvRegion;
+    private String aatSatValues;
+    private String active;
+    private String dateCreated;
+    private String lastModified;
+
+    // Getters and Setters
+    public String getWholesaler() {
+        return wholesaler;
+    }
+    public void setWholesaler(String wholesaler) {
+        this.wholesaler = wholesaler;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getNfrnRegion() {
+        return nfrnRegion;
+    }
+    public void setNfrnRegion(String nfrnRegion) {
+        this.nfrnRegion = nfrnRegion;
+    }
+    public String getTvRegion() {
+        return tvRegion;
+    }
+    public void setTvRegion(String tvRegion) {
+        this.tvRegion = tvRegion;
+    }
+    public String getAatSatValues() {
+        return aatSatValues;
+    }
+    public void setAatSatValues(String aatSatValues) {
+        this.aatSatValues = aatSatValues;
+    }
+    public String getActive() {
+        return active;
+    }
+    public void setActive(String active) {
+        this.active = active;
+    }
+    public String getDateCreated() {
+        return dateCreated;
+    }
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    public String getLastModified() {
+        return lastModified;
+    }
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+
+}
