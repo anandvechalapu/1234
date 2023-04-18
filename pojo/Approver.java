af8836268301241481b8e2f4ee0cbe6bfc21dfd5
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-18 13:01:28 +0530

    initial commit

---
+++ pojo/Approver.java
@@ -0,0 +1,157 @@
+claim status should become Rejected.
+public class Approver {
+
+private String decision;
+private String dataField;
+private String value;
+private String maker;
+private String annuity;
+private String quotation;
+private String status;
+private String document;
+private String recommendation;
+private String note;
+private String remarks;
+private String memberStatus;
+
+public Approver() {
+
+}
+
+public void setDecision(String decision) {
+ this.decision = decision;
+}
+
+public String getDecision() {
+ return this.decision;
+}
+
+public void setDataField(String dataField) {
+ this.dataField = dataField;
+}
+
+public String getDataField() {
+ return this.dataField;
+}
+
+public void setValue(String value) {
+ this.value = value;
+}
+
+public String getValue() {
+ return this.value;
+}
+
+public void setMaker(String maker) {
+ this.maker = maker;
+}
+
+public String getMaker() {
+ return this.maker;
+}
+
+public void setAnnuity(String annuity) {
+ this.annuity = annuity;
+}
+
+public String getAnnuity() {
+ return this.annuity;
+}
+
+public void setQuotation(String quotation) {
+ this.quotation = quotation;
+}
+
+public String getQuotation() {
+ return this.quotation;
+}
+
+public void setStatus(String status) {
+ this.status = status;
+}
+
+public String getStatus() {
+ return this.status;
+}
+
+public void setDocument(String document) {
+ this.document = document;
+}
+
+public String getDocument() {
+ return this.document;
+}
+
+public void setRecommendation(String recommendation) {
+ this.recommendation = recommendation;
+}
+
+public String getRecommendation() {
+ return this.recommendation;
+}
+
+public void setNote(String note) {
+ this.note = note;
+}
+
+public String getNote() {
+ return this.note;
+}
+
+public void setRemarks(String remarks) {
+ this.remarks = remarks;
+}
+
+public String getRemarks() {
+ return this.remarks;
+}
+
+public void setMemberStatus(String memberStatus) {
+ this.memberStatus = memberStatus;
+}
+
+public String getMemberStatus() {
+ return this.memberStatus;
+}
+
+public void giveDecision() {
+ // code to give decision
+}
+
+public void viewCases() {
+ // code to view cases
+}
+
+public void generateQuotation() {
+ // code to generate quotation
+}
+
+public void checkStatus() {
+ // code to check status
+}
+
+public void viewDocuments() {
+ // code to view documents
+}
+
+public void viewComments() {
+ // code to view comments
+}
+
+public void captureRemarks() {
+ // code to capture remarks
+}
+
+public void submitCase() {
+ // code to submit case
+}
+
+public void approveReject() {
+ // code to approve/reject
+}
+
+public void reverseProvision() {
+ // code to reverse provision
+}
+
+}
