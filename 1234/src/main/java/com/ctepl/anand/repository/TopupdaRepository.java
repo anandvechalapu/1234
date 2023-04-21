e8a658beb8d38ea372c782812651d5f4dd16439d
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-21 11:25:48 +0530

    Updated

--- 1234/src/main/java/com/ctepl/anand/repository/TopupdaRepository.java
+++ 1234/src/main/java/com/ctepl/anand/repository/TopupdaRepository.java
@@ -1,16 +1,17 @@
-package com.ctepl.anand.repository;
+package com.ctepl.anand.repository; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.ctepl.anand.entity.TopupdaTempEntity;
 import com.ctepl.anand.entity.TopupdaEntity;
+import com.ctepl.anand.entity.TopupdaTempEntity;
 
 @Repository
 public interface TopupdaRepository extends JpaRepository<TopupdaEntity, Long> {
 
-	TopupdaTempEntity findByTopupId(Long topupId);
-
-	TopupdaEntity save(TopupdaEntity topupdaEntity);
+    @Query("UPDATE TopupdaTempEntity t SET t.topupStatus = 'Approved', t.amountStatus = 'AmountLock', t.modifiedOn = CURRENT_TIMESTAMP WHERE t.topupId = :topupId")
+    void sendToApprove(@Param("topupId") Long topupId);
 
 }
