a517f0dd3f7c4521bc5262360075a4eaff6adb25
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-25 12:33:36 +0530

    initial commit

---
+++ aura/auraCompBuyerSetup.js
@@ -0,0 +1,38 @@
+var auraCompBuyerSetup = {
+    //Method to get all functionalities displayed on CSA setup buyer menu
+    getBuyerSetupMenu : function() {
+        //Fetch data from server
+        //Return data to the view
+    },
+    //Method to add new buyer or edit buyer functionality
+    editBuyer: function() {
+        //Validate data
+        //Save data to server
+        //Return success or failure response
+    },
+    //Method to search buyer by account number
+    searchBuyer: function() {
+        //Validate account number
+        //Search data from server
+        //Return matched data
+    },
+    //Method to change password or other confidential information by verifying account owner
+    changePassword: function() {
+        //Validate account owner
+        //Validate new password
+        //Save new password to server
+        //Return success or failure response
+    },
+    //Method to see all functionalities on buyer menu
+    viewBuyerMenu: function() {
+        //Fetch data from server
+        //Return data to the view
+    },
+    //Method to communicate with the customer through chat or call
+    communicateWithCustomer: function() {
+        //Validate customer details
+        //Connect to customer
+        //Start chat or call
+        //Return success or failure response
+    }
+}
